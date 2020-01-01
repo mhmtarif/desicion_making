abfb2ce35547aa05e3c91d763a27c123ffca5841
mhmtarif <mehmetsekercioglu@gmail.com>
2020-01-01 15:48:32 +0300

    my_sum func

--- R/hello.R
+++ R/hello.R
@@ -1,6 +1,6 @@
 # Hello, world!
 #
-# This is an example function named 'hello' 
+# This is an example function named 'hello'
 # which prints 'Hello, world!'.
 #
 # You can learn more about package authoring with RStudio at:
@@ -16,3 +16,10 @@
 hello <- function() {
   print("Hello, world!")
 }
+
+
+my_sum <- function(num1,num2) {
+  num1+num2
+}
+
+
